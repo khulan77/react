e4918a41c9b14da0da420caa5b4822591983b314
25LP7865 25LP7865 <dhulan174@gmail.com>
2025-12-02 16:00:51 +0800

    ywlaa

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div className="bg-[#111827] h-[826px] w-full flex flex-row justify-between pt-20 pr-25 pl-24">
+      <div className="flex flex-row"></div>
+    </div>
+  );
+};
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,38 @@
 export const Hero = () => {
   return (
-    <div className="w-1280 h-364 pt-24 pl-24 flex justify-between">
-      <div className="font-normal ml-20 w-xl ">
-        <div className="text-white text-6xl flex  font-bold md-15">
+    <div className="w-full h-[556px] flex flex-row justify-between pt-20 pr-25 pl-24">
+      <div className="font-normal  w-xl flex flex-col gap-12">
+        <div className="text-white text-4xl flex  font-bold ">
           Hi, I’m Tom 👋
         </div>
-        <p className="mt-4 text-white">
+        <p className="w-[600px] h-[96px] font-thin text-[16px] text-white">
           I specialize in full stack development, particularly with React.js and
           Node.js. My main goal is to create exceptional digital experiences
           that are fast, visually appealing, and accessible to everyone. With
           over 7 years of experience in web development, I continue to find joy
           in crafting innovative solutions and designs.
         </p>
-        <div className="flex gap-0.5 mt-2">
-          <img src="icon.png" alt="" />
-          Ulaanbaatar Mongolia
+        <div className="w-[600px] h-[56px] flex flex-col gap-2">
+          <div className="w-[600px] h-[24px] flex flex-row gap-2 ">
+            <img src="loc.png" alt="" />
+            <p className="w-[169px] h-[24px] font-normal text- [16px]">
+              Ulaanbaatar, Mongolia
+            </p>
+          </div>
+          <div className="w-[600px] h-[24px] flex flex-row gap-2">
+            <img src="icon1.png" alt="" />
+            Available for new projects
+          </div>
         </div>
-        <div className="flex gap-0.5 ">
-          <img src="icon1.png" alt="" />
-          Available for new projects
+        <div className="w-[600px] h-[36px] flex flex-row mt-[70px] gap-8 ">
+          <img src="github.png" alt="" />
+          <img src="twiter.png" alt="" />
+          <img src="figma.png" alt="" />
         </div>
       </div>
+
       <div>
-        <img src="tom.png" alt="" />
+        <img src="tomtomi.png" alt="" />
       </div>
     </div>
   );
